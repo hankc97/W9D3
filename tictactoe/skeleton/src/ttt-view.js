dd8c876e49d5b2a40e50906904aa889c5d736eeb
Hank <hankchen1997@yahoo.com>
2021-02-24 17:35:03 -0500

    added

--- tictactoe/skeleton/src/ttt-view.js
+++ tictactoe/skeleton/src/ttt-view.js
@@ -7,18 +7,32 @@ class View {
     }
     
     bindEvents() {
-        this.currentPlayer = this.game.currentPlayer
-        this.pos = $('ul').on('click', 'li', this.clickHandler.bind(this))
-        this.game.playMove(this.pos)
+        $('ul').on('click', 'li', this.clickHandler.bind(this))
     }
 
     clickHandler(e) {
         let currentTarget = $(e.currentTarget)
-        debugger
-        return currentTarget.data('rowCol')
+        this.makeMove(currentTarget)
     }
     
-    makeMove($square) {}
+    makeMove($square) {
+
+        this.currentPlayer = this.game.currentPlayer // [x, o]
+        this.pos = $square.data("rowCol")
+
+        this.game.playMove(this.pos)
+        $square.addClass(`${this.currentPlayer}`);
+
+        if (this.game.isOver()) {
+            if (this.game.winner()) {
+                $(`li.${this.currentPlayer}`).attr("class", "winner").addClass(`${this.currentPlayer}`)
+                alert(`${this.game.winner()} has won!`);
+            } else {
+                alert('NO ONE WINS!');
+            }
+        }
+
+    }
     
     setupBoard() {
         const $ul = $("<ul></ul>") 
@@ -26,7 +40,7 @@ class View {
         for(let row = 0; row < 3; row++){
             for (let col = 0; col < 3; col++) {
                 const $li = $("<li></li>");
-                $li.data('rowCol', [row, col]),
+                $li.data('rowCol', [row, col]);
                 $ul.append($li);
             }
         }
